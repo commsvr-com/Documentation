dc3b6f2901b4d92d871910626e98a534db1f9785
mzbrzezny <7295087+maciej-progtech@users.noreply.github.com>
2011-05-16 07:29:28 +0000

    [CWW-2720] ProductHelp: Release 11.05 update of create tag script (change: inclusion of EX02-MAML) Issue #: 2720

--- Scripts/create_branch.cmd
+++ Scripts/create_branch.cmd
@@ -12,6 +12,7 @@ if "%branchtype%"=="" goto setbranch
 svn mkdir svn://svnserver.hq.cas.com.pl/VS/%branchtype%/PR34-Documentation/%1  -m "created new %branchtype%  %1 (%branchtype% folder)"
 rem svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/ImageLibrary svn://svnserver.hq.cas.com.pl/VS/%branchtype%/PR34-Documentation/%1/ImageLibrary -m "created new PR34-Documentation %branchtype% %1 (project ImageLibrary)"
 rem svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommonBinaries svn://svnserver.hq.cas.com.pl/VS/%branchtype%/PR34-Documentation/%1/CommonBinaries -m "created new PR34-Documentation %branchtype% %1 (project CommonBinaries)"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/EX02-MAML svn://svnserver.hq.cas.com.pl/VS/%branchtype%/PR34-Documentation/%1/EX02-MAML -m "created new EX02-MAML %branchtype% %1 (project PR34-Documentation)"
 svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/PR34-Documentation svn://svnserver.hq.cas.com.pl/VS/%branchtype%/PR34-Documentation/%1/PR34-Documentation -m "created new PR34-Documentation %branchtype% %1 (project PR34-Documentation)"
 
 goto EXIT
